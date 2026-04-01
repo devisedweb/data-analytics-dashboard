258687ead5bc0486da12e5ad9ece0dfdeff9ef45
DevisedWeb <contact@devisedweb.com>
2026-04-01 17:07:00 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // javascript
+
+const menuBtn = document.querySelector(".menu-btn");
+const backBtn = document.querySelector(".back-btn");
+const menu = document.querySelector("nav");
+
+menuBtn.addEventListener("click", function () {
+  menu.style.transform = "translateX(0)";
+});
+
+backBtn.addEventListener("click", function () {
+  menu.style.transform = "translateX(-100%)";
+});
